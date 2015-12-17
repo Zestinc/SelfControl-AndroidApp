d88e9d10fb46ab0a227af2b8dd42ff2a012bd686
Zestinc <zestinc32@gmail.com>
2015-12-17 02:10:42 -0800

    Interface change + Data
    1. Change event button to challenge + break buttons.
    2. Delete selectActivity
    3. Update Lv. in MainActivity
    
    当前：challenge时间计时

--- app/src/main/java/com/example/zestinc/selfcontrol/ChallengeActivity.java
+++ app/src/main/java/com/example/zestinc/selfcontrol/ChallengeActivity.java
@@ -1,7 +1,10 @@
 package com.example.zestinc.selfcontrol;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChallengeActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class ChallengeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge);
+
+        Button button = (Button) findViewById(R.id.successButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences("challendge", 0);
+                Long beginTime = sharedPreferences.getLong("Time", -1);
+                Long endTime = System.currentTimeMillis();
+                if(beginTime == -1){
+//                    System.out.print();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/zestinc/selfcontrol/MainActivity.java
+++ app/src/main/java/com/example/zestinc/selfcontrol/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.zestinc.selfcontrol;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button)findViewById(R.id.eventButton);
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, SelectActivity.class);
+        Button button = (Button) findViewById(R.id.challengeButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Record begin time.
+                SharedPreferences sharedPreferences = getSharedPreferences("challendge", 0);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putLong("time", System.currentTimeMillis());
+                editor.commit();
+
+                //Go into challenge activity
+                Intent intent = new Intent(MainActivity.this, ChallengeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button = (Button) findViewById(R.id.breakButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RelaxActivity.class);
                 startActivity(intent);
             }
         });
@@ -27,8 +48,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
+
         View decorView = getWindow().getDecorView();
-        // Hide the status bar.
+        // Hide the status bar.D
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
         //Hide ActionBar (always do it when hiding the status bar)
@@ -39,5 +61,11 @@ public class MainActivity extends AppCompatActivity {
         //Set title center
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.actionbar);
+
+        //Update the showing of Lv.
+        SharedPreferences sharedPreferences = getSharedPreferences("person", 0);
+        int level = sharedPreferences.getInt("level", 0);
+        TextView textView = (TextView) findViewById(R.id.levelTitle);
+        textView.setText("Lv." + Integer.toString(level));
     }
 }
