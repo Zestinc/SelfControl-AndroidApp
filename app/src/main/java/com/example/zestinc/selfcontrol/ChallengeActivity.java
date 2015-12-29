9c22d563fe32a5768ee8b9795fdac140a52515c6
Zestinc <zestinc32@gmail.com>
2015-12-28 17:00:56 -0800

    challenge experience handle
    Implement the time to experience function.

--- app/src/main/java/com/example/zestinc/selfcontrol/ChallengeActivity.java
+++ app/src/main/java/com/example/zestinc/selfcontrol/ChallengeActivity.java
@@ -3,6 +3,7 @@ package com.example.zestinc.selfcontrol;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,11 +19,52 @@ public class ChallengeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 SharedPreferences sharedPreferences = getSharedPreferences("challendge", 0);
-                Long beginTime = sharedPreferences.getLong("Time", -1);
+                Long beginTime = sharedPreferences.getLong("time", -1);
                 Long endTime = System.currentTimeMillis();
                 if(beginTime == -1){
-//                    System.out.print();
+//                    获取开始时间失败，输出程序错误信息
+                    Log.e("error", " 获取开始时间失败");
                 }
+                else {
+                    Long duration = endTime - beginTime;
+                    Log.d("time to exp.", duration.toString());
+                    if(duration > 24*60*60*1000){
+                        //持续时间超过1天，惊为天人
+                    }
+                    else{
+                        //正常计算时间转化为的经验值
+                        /*
+                        经验值计算方式（暂定）： 1EXP/MIN
+                        自控力上限：MaxSelfControlValue = 20 + Level
+                         */
+                        Long currentExp = sharedPreferences.getLong("currentExp", 0);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        currentExp += duration/1000/60;
+                        int level = sharedPreferences.getInt("level", 0);
+                        while(currentExp >= (100 + level * 20)){
+                            /*
+                            ！！！！！！！！！
+                            ！！！！！！！！
+                            ！！！！！！
+                            ！！！！！
+                            ！！！！
+                            ！！！
+                            ！！
+                            ！
+                            此处应有升级恭喜提示
+                             */
+                            currentExp -= (100 + level*20);
+                            level += 1;
+                            editor.putInt("level", level);
+                        }
+                        editor.putLong("currentExp", currentExp);
+                        editor.commit();
+                    }
+                }
+                finish();
+                /*
+                提示信息，休息，温馨提示
+                 */
             }
         });
     }
